18e963ed973be97cd8bf2cbcb7e54cf5dbd38c8b
João Paulo Dias Estevão <92491011+joaodest@users.noreply.github.com>
2026-09-02 13:41:30 -0300

    Add: crawler-log-folder - submission-5

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-5.py
@@ -0,0 +1,23 @@
+from enum import StrEnum
+import re
+class Ops(StrEnum):
+    UP = "../"
+    STAY = "./"
+    
+    
+
+class Solution:
+    
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+        
+        for log in logs:
+            if log == Ops.STAY:
+                continue
+            elif log == Ops.UP:
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(log)
+            
+        return len(stack)
